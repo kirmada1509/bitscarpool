c471e4de2bacfb8eec9d5022b7b8cd7cb2141c71
kirmada150905 <174258997+kirmada150905@users.noreply.github.com>
2025-04-04 18:22:08 +0530

    Update Tailwind CSS configuration to include components directory and fix typo in color definition

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./app/**/*.{js,jsx,ts,tsx}"],
+    content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     presets: [require("nativewind/preset")],
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Poppins", "sans-serif"], // Default font (Poppins)
-                poppins: ["Poppins", "sans-serif"],
-                roboto: ["Roboto", "sans-serif"], // Added Roboto
-            },
-            colors: {
-                primary: "#45EA69",
-                black: "#1D1D1F",
-                black_1: "#313133",
-                black_2: "#3D3C3F",
-                black_3: "#8C8A93",
-                whtie: "#FFFFFF",
-            },
+      extend: {
+        fontFamily: {
+          sans: ["Poppins", "sans-serif"],
+          poppins: ["Poppins", "sans-serif"],
+          roboto: ["Roboto", "sans-serif"],
         },
+        colors: {
+          primary: "#45EA69",
+          black: "#1D1D1F",
+          black_1: "#313133",
+          black_2: "#3D3C3F",
+          black_3: "#8C8A93",
+          white: "#FFFFFF", // typo fixed
+        },
+      },
     },
     plugins: [],
-};
+  };
+  
